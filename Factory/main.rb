5eb2bd44b256e158becaaa78ae0258fab37227e1
smakino <re.verage.lalala@gmail.com>
2014-05-03 00:46:56 +0900

    delete comment

--- Factory/main.rb
+++ Factory/main.rb
@@ -4,12 +4,6 @@ require "./factory"
 require "./id_card_factory"
 require "./id_card"
 
-# ファクトリメソッド
-# factory methodではインスタンスの作り方をスーパークラス側で決める
-# 具体的なクラス名などは定めない
-# 具体的な肉付けは全てサブクラスで行う
-# これによって、インスタンスの生成の枠組みと実際の
-# インスタンス生成のクラスを分けてがんが得られる
 
 
 factory = IDCardFactory.new
